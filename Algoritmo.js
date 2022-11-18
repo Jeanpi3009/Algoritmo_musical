52b699a88359d7d24cc248dc0860cec15d060c6a
Jeanpierre <jeanpierreloaizaleon@gmail.com>
2022-11-17 21:04:13 -0500

    Cambios en el algoritmo

--- Algoritmo.js
+++ Algoritmo.js
@@ -1,7 +1,7 @@
 //creamos una funcion en donde declaramos una lista con las palabras
-function ChoirSong(numberUser) {
-    if (typeof (numberUser) === "number") {
-        if (numberUser > 0 && numberUser <= 4) {
+function ChoirSong(Number) {
+    if (typeof (Number) === "number") {
+        if (Number > 0 && Number <= 4) {
             let song1 = [' Mami ', ' Gata ', ' Perra ', ' Zorra ', ' Chica '];
             let song2 = [' yo quiero ', ' vamos a ', ' yo voy a ', ' yo quiero ', 'yo vengo a '];
             let song3 = [' castigarte ', ' cogerte ', ' encenderte', ' darte', ' azotarte'];
@@ -9,17 +9,17 @@ function ChoirSong(numberUser) {
             let song5 = [' hasta que salga el sol ', ' toda la noche ', ' hasta el amanecer ', ' hasta mañana ', ' todo el día '];
             let song6 = [' sin miedo ', ' sin anestesia ', ' en el piso ', ' contra la pared ', ' sin compromiso '];
             //utilizamos el metodo Random para generar una lista al azar
-            var random = Math.round(Math.random() * (4 - 1) + 1);
+            var random = Math.round(Math.random() * (5 - 1));
             let rhyme6 = song1[random];
-            var random = Math.round(Math.random() * (4 - 1) + 1);
+            var random = Math.round(Math.random() * (5 - 1));
             let rhyme7 = song2[random];
-            var random = Math.round(Math.random() * (4 - 1) + 1);
+            var random = Math.round(Math.random() * (5 - 1));
             let rhyme8 = song3[random];
-            var random = Math.round(Math.random() * (4 - 1) + 1);
+            var random = Math.round(Math.random() * (5 - 1));
             let rhyme9 = song4[random];
-            var random = Math.round(Math.random() * (4 - 1) + 1);
+            var random = Math.round(Math.random() * (5- 1));
             let rhyme10 = song5[random];
-            var random = Math.round(Math.random() * (4 - 1) + 1);
+            var random = Math.round(Math.random() * (5 - 1));
             let rhyme11 = song6[random];
 
             console.log(rhyme6, rhyme7, rhyme8, rhyme9, rhyme10, rhyme11);
@@ -30,7 +30,7 @@ function ChoirSong(numberUser) {
             var n = 0;
             do {
 
-                var random = Math.round(Math.random() * (4 - 1) + 1);
+                var random = Math.round(Math.random() * (5 - 1));
                 if ((random != uno) && (random != dos) && (random != tres)) {
                     n++;
                     if (n == 1) {
@@ -48,11 +48,11 @@ function ChoirSong(numberUser) {
             let rhyme1 = song3[uno];
             let rhyme2 = song3[dos];
             let rhyme3 = song3[tres];
-            var random = Math.round(Math.random() * (4 - 1) + 1);
+            var random = Math.round(Math.random() * (5 - 1));
             let rhyme4 = song5[random];
-            var random = Math.round(Math.random() * (4 - 1) + 1);
+            var random = Math.round(Math.random() * (5 - 1));
             let rhyme5 = song4[random];
-            for (var a = 0; a < numberUser; a++) {
+            for (var a = 0; a < Number; a++) {
 
                 console.log(rhyme1, rhyme2, rhyme3, rhyme4, rhyme5);
             }
